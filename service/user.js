4a0bb10f137c9f724b51d6bed8e828cab4942680
Sascha Dobschal <s.dobschal@tdsoftware.de>
2020-09-28 08:34:50 +0200

    new config value

--- service/user.js
+++ service/user.js
@@ -58,7 +58,7 @@ function updateUserValues(user, castles, warehouses) {
                     WHERE id = ?`).run(hammerPerMinute, level, user.id);
     }
     if (warehouses) {
-        const maxHammers = Math.max(config.MAX_HAMMERS, (warehouses.length) * config.AVERAGE_ROADS_PER_CASTLE * priceMultiplier);
+        const maxHammers = Math.max(config.MAX_HAMMERS, (warehouses.length + 1) * config.AVERAGE_ROADS_PER_CASTLE * priceMultiplier);
         db.prepare(`UPDATE user
                     SET max_hammers = ?
                     WHERE id = ?`).run(maxHammers, user.id);
