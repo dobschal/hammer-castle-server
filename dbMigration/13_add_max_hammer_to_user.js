bef7beb1d12aa9038caa6751c18b9c8f44714fb6
Sascha Dobschal <s.dobschal@tdsoftware.de>
2020-10-01 16:57:20 +0200

    renamed migration scripts

--- dbMigration/13_add_max_hammer_to_user.js
+++ dbMigration/13_add_max_hammer_to_user.js
@@ -2,7 +2,7 @@ const db = require("../lib/database");
 
 module.exports = function (app) {
     db.prepare(`ALTER TABLE user
-        ADD max_hammers INTEGER DEFAULT 180;`).run();
-    db.prepare(`ALTER TABLE user
+        ADD max_hammers INTEGER DEFAULT 180;
+    ALTER TABLE user
         ADD last_active_at INTEGER DEFAULT 0;`).run();
 };
