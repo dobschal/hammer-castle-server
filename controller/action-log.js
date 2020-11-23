323f5cc09f0fd4b652fcb5654df7b4f46cc7a792
dobschal <s.dobschal@tdsoftware.de>
2020-11-23 10:27:35 +0100

    added log

--- controller/action-log.js
+++ controller/action-log.js
@@ -4,11 +4,14 @@ const hasUserRole = require("../filter/hasUserRole");
 const actionLogService = require("../service/actionLogService");
 const schema = require("../lib/schema");
 const userService = require("../service/userService");
+const timer = require("../lib/timer");
 
 router.get("/", hasUserRole(["USER"]), function (req, res) {
+    timer.start("GET_ACTION_LOG");
     const user = userService.currentUser(req);
     schema.is(user, "entity/User");
     res.send(actionLogService.getAllOfUser(user));
+    timer.end("GET_ACTION_LOG", user.username);
 });
 
 module.exports = router;
